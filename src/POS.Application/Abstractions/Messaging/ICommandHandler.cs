2b3b1f6139f1e1bbd4db9c39915cbb336250d230
nguyenan12 <nguyenann1204@gmail.com>
2026-09-03 20:21:01 +0700

    fix: make ICommandHandler inherit IRequestHandler

--- src/POS.Application/Abstractions/Messaging/ICommandHandler.cs
+++ src/POS.Application/Abstractions/Messaging/ICommandHandler.cs
@@ -1,21 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using MediatR;
 using POS.Domain.Common;
 
-namespace POS.Application.Abstractions.Messaging
+namespace POS.Application.Abstractions.Messaging;
+
+public interface ICommandHandler<in TCommand> : IRequestHandler<TCommand, Result>
+    where TCommand : ICommand
 {
+  new Task<Result> Handle(TCommand command, CancellationToken cancellationToken);
+}
 
-  public interface ICommandHandler<in TCommand>
-      where TCommand : ICommand
-  {
-    Task<Result> Handle(TCommand command, CancellationToken cancellationToken);
-  }
-
-  public interface ICommandHandler<in TCommand, TResponse>
-      where TCommand : ICommand<TResponse>
-  {
-    Task<Result<TResponse>> Handle(TCommand command, CancellationToken cancellationToken);
-  }
-
+public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, Result<TResponse>>
+    where TCommand : ICommand<TResponse>
+{
+  new Task<Result<TResponse>> Handle(TCommand command, CancellationToken cancellationToken);
 }
