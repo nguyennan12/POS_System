9c412247fb0d6c22ccefdf205961dec8cd041bca
2uanx <tkxike@gmail.com>
2026-09-19 22:45:01 +0700

    IRequirePermission for shift

--- src/POS.Application/UseCases/Shifts/Queries/GetShiftById/GetShiftByIdQuery.cs
+++ src/POS.Application/UseCases/Shifts/Queries/GetShiftById/GetShiftByIdQuery.cs
@@ -1,6 +1,10 @@
+using POS.Application.Abstractions.Auth;
 using POS.Application.Abstractions.Messaging;
 using POS.Application.UseCases.Shifts.Commands.OpenShift;
 
 namespace POS.Application.UseCases.Shifts.Queries.GetShiftById;
 
-public record GetShiftByIdQuery(Guid ShiftId) : IQuery<ShiftDto>;
+public record GetShiftByIdQuery(Guid ShiftId) : IQuery<ShiftDto>, IRequirePermission
+{
+    public string RequiredPermission => "shifts:read";
+}
---
+++ src/POS.Infrastructure/Persistence/Migrations/20260919151146_FixPromotionTargetAndStockInVoucher.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace POS.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPromotionTargetAndStockInVoucher : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_promotion_targets_promotions_promotion_id1",
+                table: "promotion_targets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_roles_store_id_name",
+                table: "roles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_promotion_targets_promotion_id1",
+                table: "promotion_targets");
+
+            migrationBuilder.DropColumn(
+                name: "promotion_id1",
+                table: "promotion_targets");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_roles_name",
+                table: "roles",
+                column: "name",
+                unique: true,
+                filter: "store_id IS NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_roles_store_id_name",
+                table: "roles",
+                columns: new[] { "store_id", "name" },
+                unique: true,
+                filter: "store_id IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_roles_name",
+                table: "roles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_roles_store_id_name",
+                table: "roles");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "promotion_id1",
+                table: "promotion_targets",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_roles_store_id_name",
+                table: "roles",
+                columns: new[] { "store_id", "name" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_promotion_targets_promotion_id1",
+                table: "promotion_targets",
+                column: "promotion_id1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_promotion_targets_promotions_promotion_id1",
+                table: "promotion_targets",
+                column: "promotion_id1",
+                principalTable: "promotions",
+                principalColumn: "id");
+        }
+    }
+}
